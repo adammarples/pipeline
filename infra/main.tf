e07666f8e01dc2d8ab4d7e215fd23f227bfa45f5
adammarples <adammarples@gmail.com>
2023-01-22 14:30:43 +0000

    give orchestrator a role with bigquery jobs create

--- infra/main.tf
+++ infra/main.tf
@@ -3,6 +3,19 @@ resource "google_service_account" "orchestrator" {
   display_name = "Orchestrator"
 }
 
+resource "google_project_iam_member" "orchestrator_binding" {
+  project = var.gcp_project
+  role    = google_project_iam_custom_role.orchestrator_role.id
+  member  = google_service_account.orchestrator.member
+}
+
+resource "google_project_iam_custom_role" "orchestrator_role" {
+  role_id     = "orchestrator"
+  title       = "Orchestrator Role"
+  description = "To be used by airflow/dbt to manage the database"
+  permissions = ["bigquery.jobs.create"]
+}
+
 resource "google_bigquery_dataset" "staging_dataset" {
   dataset_id                  = "staging"
   friendly_name               = "staging"
