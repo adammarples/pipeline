8a5e579076942e041598bd4671aa4b9a30dc2e36
adammarples <adammarples@gmail.com>
2023-01-23 12:34:36 +0000

    raw devices table

---
+++ infra/raw_devices_table.tf
@@ -0,0 +1,40 @@
+resource "google_bigquery_table" "default" {
+  dataset_id = google_bigquery_dataset.raw_dataset.dataset_id
+  table_id   = "devices"
+
+  external_data_configuration {
+    autodetect    = false
+    compression   = "NONE"
+    source_format = "CSV"
+
+    source_uris = [
+      "gs://${google_storage_bucket.raw_bucket.name}/devices.csv",
+    ]
+  }
+
+  labels = {
+    env = "default"
+  }
+
+  schema = <<EOF
+[
+  {
+    "name": "device_id",
+    "type": "STRING",
+    "mode": "NULLABLE",
+    "description": ""
+  },{
+    "name": "device_type",
+    "type": "STRING",
+    "mode": "NULLABLE",
+    "description": ""
+  },{
+    "name": "device_model",
+    "type": "STRING",
+    "mode": "NULLABLE",
+    "description": ""
+  }
+]
+EOF
+
+}
