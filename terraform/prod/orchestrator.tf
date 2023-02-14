43b5c6a35032a1e7aff14ce6adee2921c0e21a50
adammarples <adammarples@gmail.com>
2023-02-14 20:30:37 +0000

    unhack terraform, expired key

--- terraform/prod/orchestrator.tf
+++ terraform/prod/orchestrator.tf
@@ -5,22 +5,7 @@ resource "google_service_account" "orchestrator" {
 
 resource "google_project_iam_member" "orchestrator_role_binding" {
   project = var.gcp_project
-  role    = google_project_iam_custom_role.orchestrator_role.id
+  role    = "roles/bigquery.admin"
   member  = google_service_account.orchestrator.member
 }
 
-resource "google_project_iam_member" "orchestrator_iam_token_creator_binding" {
-  project = var.gcp_project
-  role    = "roles/iam.serviceAccountTokenCreator"
-  member  = google_service_account.orchestrator.member
-}
-
-resource "google_project_iam_custom_role" "orchestrator_role" {
-  role_id     = "orchestrator"
-  title       = "Orchestrator Role"
-  description = "To be used by airflow/dbt to manage the database"
-  permissions = [
-    "bigquery.jobs.create",
-    "bigquery.datasets.create",
-  ]
-}
