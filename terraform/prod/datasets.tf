ee970a5f929f00a8f67ad4ebd4812ecb0802ef34
adammarples <adammarples@gmail.com>
2023-02-14 22:11:44 +0000

    remove vault dataset

--- terraform/prod/datasets.tf
+++ terraform/prod/datasets.tf
@@ -33,21 +33,3 @@ resource "google_bigquery_dataset" "staging_dataset" {
   }
 
 }
-
-resource "google_bigquery_dataset" "vault_dataset" {
-  dataset_id                  = "vault"
-  friendly_name               = "vault"
-  description                 = "Curated dataset for vault data"
-  location                    = "EU"
-  default_table_expiration_ms = 3600000
-
-  labels = {
-    env = "default"
-  }
-
-  access {
-    role          = "OWNER"
-    user_by_email = google_service_account.orchestrator.email
-  }
-
-}
